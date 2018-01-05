ddd9baa090dc199884b943fc8c6b7022fb8d54fb
Maxim Korolkov <korolkovm@gmail.com>
2018-01-05 22:16:50 +0300

    Sample of linked list implementation added.

---
+++ Theory/DataStructs/LinkedList.cs
@@ -0,0 +1,138 @@
+﻿//
+// (c) 2018 Maxim Korolkov
+//
+// Theory project
+//
+
+using System;
+
+namespace Theory
+{
+    internal class LinkedList<T> where T : IComparable<T>
+    {
+        private const string OutOfRangeMsg = "LinkedList: index={0} out of range or list is empty.";
+
+        private Node head;
+        private int count;
+        
+        public int Count => count;
+
+        private class Node
+        {
+            private T data;
+            private Node next;
+            
+            public T Data
+            {
+                get => data;
+                set => data = value;
+            }
+
+            public Node Next
+            {
+                get => next;
+                set => next = value;
+            }
+
+            public Node(T data) : this(data, null) {}
+
+            public Node(T data, Node next)
+            {
+                this.data = data;
+                this.next = next;
+            }
+        }
+        
+        public void Add(T data)
+        {
+            if (head == null)
+            {
+                head = new Node(data);
+            }
+            else
+            {
+                var currentNode = head;
+
+                while (currentNode.Next != null)
+                {
+                    currentNode = currentNode.Next;
+                }
+
+                currentNode.Next = new Node(data);
+            }
+
+            ++count;
+        }
+
+        public void Remove(T data)
+        {
+            if (head == null)
+            {
+                return;
+            }
+
+            var currentNode = head;
+            Node previousNode = null;
+            
+            while (currentNode != null && currentNode.Data.CompareTo(data) != 0)
+            {
+                previousNode = currentNode;
+                currentNode = currentNode.Next;
+            }
+
+            if (currentNode != null)
+            {
+                if (previousNode != null)
+                {
+                    previousNode.Next = currentNode.Next;
+                }
+                else
+                {
+                    head = currentNode.Next;
+                }
+
+                --count;
+            }
+        }
+
+        public void Insert(int index, T data)
+        {
+            if (index - 1 < 0)
+            {
+                head = new Node(data, head);
+            }
+            else
+            {
+                var node = GetNodeAt(index - 1);
+                node.Next = new Node(data, node.Next);
+            }
+
+            ++count;
+        }
+
+        public T this[int index]
+        {
+            get => GetNodeAt(index).Data;
+            set => GetNodeAt(index).Data = value;
+        }
+
+        private Node GetNodeAt(int index)
+        {
+            if (index >= count || index < 0 || head == null)
+            {
+                throw (new IndexOutOfRangeException(String.Format(OutOfRangeMsg, index)));
+            }
+
+            var currentNode = head;
+            var currentCount = 0;
+
+            while (currentNode != null && currentCount != index)
+            {
+                currentNode = currentNode.Next;
+                ++currentCount;
+            }
+
+            return currentNode;
+        }
+    }
+}
--- Theory/Program.cs
+++ Theory/Program.cs
@@ -5,6 +5,7 @@
 //
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Theory
@@ -13,11 +14,58 @@ namespace Theory
     {
         static void Main(string[] args)
         {
-            var unsorted = new [] { 12, 5, 3, 14, 8, 11, 7, 4, 89, 12, 13, 24, 5, 78 };
+            LinkedListDemo();
+            ArrayDemo();
+        }
+
+        private static void LinkedListDemo()
+        {
+            var list = new LinkedList<string>();
+
+            list.Add("Maxer");
+            list.Add("Lomaster");
+            list.Add("Flomaster");
+            EnumerateLinkedList(list, "Original");
+
+            list.Insert(0, "First");
+            list.Insert(list.Count, "Last");
+            EnumerateLinkedList(list, "Inserted: First, Last");
+
+            list.Remove("Lomaster");
+            EnumerateLinkedList(list, "Removed: Lomaster");
+
+            list[2] = "Red Flomaster";
+            EnumerateLinkedList(list, "Updated: Flomaster -> Red Flomaster");
+
+            try
+            {
+                list.Insert(66, "");
+            }
+            catch (IndexOutOfRangeException err)
+            {
+                EnumerateLinkedList(list, "Exception was thrown");
+                Console.WriteLine(err.Message);
+            }
+        }
+        
+        private static void ArrayDemo()
+        {
+            var unsorted = new[] { 12, 5, 3, 14, 8, 11, 7, 4, 89, 12, 13, 24, 5, 78 };
             var sorted = Sort<int>.Quick(unsorted.ToArray());
-            
+
             PrintArray(unsorted, nameof(unsorted));
             PrintArray(sorted, nameof(sorted));
+        }
+
+        private static void EnumerateLinkedList<T>(LinkedList<T> list, string message = "")
+            where T : IComparable<T>
+        {
+            Console.WriteLine("--- " + message + " ---");
+
+            for (var i = 0; i < list.Count; ++i)
+            {
+                Console.WriteLine(list[i].ToString() + " ");
+            }
         }
 
         private static void PrintArray<T>(T[] array, string message = "")
